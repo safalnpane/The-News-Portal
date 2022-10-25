151cb7ae8ae5c0419cc6e889b7ae6defbe1b0df2
Safal Neupane <safal.npane@gmail.com>
2022-10-25 22:48:11 +1100

    Initial commit

---
+++ The News Portal/The_News_PortalApp.swift
@@ -0,0 +1,17 @@
+//
+//  The_News_PortalApp.swift
+//  The News Portal
+//
+//  Created by Safal Neupane on 25/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct The_News_PortalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
